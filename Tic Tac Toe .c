ae5e9a3e5f3ee4c949bfda591f0a09b7d3cbf5a1
Manvi Jha <drprofmjha@gmail.com>
2020-10-02 12:46:44 +0530

    Update Tic Tac Toe .c

--- Tic Tac Toe .c
+++ Tic Tac Toe .c
@@ -1,23 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
-
-int main() {
-    char nm[15];
+#include <time.h>
+ char nm[15];
     char am[15];
     char arr[3][3];
     char P1 ='O';
     char P2 ='X';
-    arr[0][0] = '1';
-    arr[0][1] = '2';
-    arr[0][2] = '3';
-    arr[1][0] = '4';
-    arr[1][1] = '5';
-    arr[1][2] = '6';
-    arr[2][0] = '7';
-    arr[2][1] = '8';
-    arr[2][2] = '9';
+    
     int i = 0 ;
     int r1;
     int r2;
@@ -207,6 +196,27 @@ void winp1() {
 
 }
 
+void delay(int milliseconds)
+{
+    long pause;
+    clock_t now,then;
+
+    pause = milliseconds*(CLOCKS_PER_SEC/1000);
+    now = then = clock();
+    while( (now-then) < pause )
+        now = clock();
+}
+
+int main() {
+    arr[0][0] = '1';
+    arr[0][1] = '2';
+    arr[0][2] = '3';
+    arr[1][0] = '4';
+    arr[1][1] = '5';
+    arr[1][2] = '6';
+    arr[2][0] = '7';
+    arr[2][1] = '8';
+    arr[2][2] = '9';
 
 
 
@@ -215,21 +225,21 @@ void winp1() {
     printf("\nPlease Enter Name of Player 2\n");
     scanf("%s", &am);
     printf("\t\t THE RULES\n\n");
-    sleep(2);
+    delay(2000);
     printf("This is a Game of Tic Tac.\n ");
-    sleep(1);
+    delay(1000);
     printf("It is for two players, each player gets a chance in which he enters the cell number where he wants to put his mark.\n");
-    sleep(1);
+    delay(1000);
     printf("The player who gets three of his marks in a straight line - Horizontal/vertical/diagonal- Wins the game.\n");
-    sleep(1);
+    delay(1000);
     printf("If you try to mark an alredy marked position you will lose that chance!");
-    sleep(1);
+    delay(1000);
     printf("The cell numbers are given here for refrence-\n");
-    sleep(1);
+    delay(1000);
     printf("\t\t\t %c \t | \t %c \t | \t %c \t | \n \t\t -----------------------------------------------------\n", arr[0][0], arr[0][1], arr[0][2]);
     printf("\t\t\t %c \t | \t %c \t | \t %c \t | \n \t\t-----------------------------------------------------\n", arr[1][0], arr[1][1], arr[1][2]);
     printf("\t\t\t %c \t | \t %c \t | \t %c \t | \n \t\t------------------------------------------------------\n", arr[2][0], arr[2][1], arr[2][2]);
-    sleep(1);
+    delay(1000);
     printf("\t\t Let's Begin !!");
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
@@ -243,7 +253,7 @@ void winp1() {
         for(int chance = 1; chance < 9 && aj!=1 ; chance++){
            if (chance % 2 == 1) {
                printf("Player 1's Turn\n");
-               sleep(1);
+               delay(1000);
                 printf("Enter Your position of Mark\n");
                 scanf("%d", &r1);
                 movep1(r1);
@@ -252,7 +262,7 @@ void winp1() {
            }
            else {
                printf("Player 2's Turn\n");
-               sleep(1);
+               delay(1000);
                printf("Enter Your position of Mark\n");
                scanf("%d", &r2);
                movep2(r2);
